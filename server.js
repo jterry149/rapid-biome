f513bfbd2b864936672d89798e383bb66ba0a116
JTerry <jessica.terry79@yahoo.com>
2018-12-12 11:22:33 -0600

    added contact page CSS

--- server.js
+++ server.js
@@ -68,22 +68,24 @@ app.post('/contact', function (req, res) {
       port: 465,
       secure: true,
       auth: {
-        user: GMAIL_USER,
-        pass: GMAIL_PASS
+        user: process.env.GMAIL_USER,
+        pass: process.env.GMAIL_PASS
       }
     });
     mailOpts = {
       from: req.body.name + ' &lt;' + req.body.email + '&gt;',
-      to: GMAIL_USER,
+      to: process.env.GMAIL_USER,
       subject: 'New message from contact form at fasting companion',
       text: `${req.body.name} (${req.body.email}) says: ${req.body.message}`
     };
     smtpTrans.sendMail(mailOpts, function (error, response) {
-      if (error) {
-        res.render('contact-failure');
+      if (error) 
+      {
+        res.render('contactfailure');
       }
-      else {
-        res.render('contact-success');
+      else 
+      {
+        res.render('contactsuccess');
       }
     });
   });
